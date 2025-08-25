fafc04cbacd736444414da3a20dea3037706886c
rivercityinternetgroup <github@primary.net>
2025-08-26 01:02:07 +0800

    Updated/Added simply-static-1-1756139847

--- site/wp-content/plugins/elementor-pro/assets/js/load-more.f5ecc1c66872d0bd2d17.bundle.js
+++ site/wp-content/plugins/elementor-pro/assets/js/load-more.f5ecc1c66872d0bd2d17.bundle.js
@@ -1,4 +1,5 @@
-/*! elementor-pro - v3.30.0 - 01-07-2025 */
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html><body><p>/*! elementor-pro - v3.31.0 - 10-08-2025 */
 "use strict";
 (self["webpackChunkelementor_pro"] = self["webpackChunkelementor_pro"] || []).push([["load-more"],{
 
@@ -6,7 +7,7 @@
 /*!************************************************************************!*\
   !*** ../modules/loop-builder/assets/js/frontend/handlers/load-more.js ***!
   \************************************************************************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+/***/ ((__unused_webpack_module, exports, __webpack_require__) =&gt; {
 
 
 
@@ -223,4 +224,4 @@ exports["default"] = LoadMore;
 /***/ })
 
 }]);
-//# sourceMappingURL=load-more.f5ecc1c66872d0bd2d17.bundle.js.map
+//# sourceMappingURL=load-more.f5ecc1c66872d0bd2d17.bundle.js.map</style></p></body></html>
