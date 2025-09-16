402f33243b4cbceceb3aebe9c6cc50b5e9ffc9b1
rivercityinternetgroup <github@primary.net>
2025-09-17 01:34:36 +0800

    Updated/Added simply-static-1-1758042052

--- site/wp-content/plugins/elementor/assets/js/packages/editor-app-bar/editor-app-bar.strings.js
+++ site/wp-content/plugins/elementor/assets/js/packages/editor-app-bar/editor-app-bar.strings.js
@@ -3,18 +3,18 @@ __( 'Help', 'elementor' );
 __( 'More', 'elementor' );
 __( 'Elementor Logo', 'elementor' );
 __( 'Integrations', 'elementor' );
-__( 'User Preferences', 'elementor' );
 __( 'Theme Builder', 'elementor' );
-__( 'Structure', 'elementor' );
+__( 'User Preferences', 'elementor' );
 __( 'Site Settings', 'elementor' );
-__( 'Save Changes', 'elementor' );
+__( 'Structure', 'elementor' );
 __( 'Switch Device', 'elementor' );
 // translators: %s: Breakpoint label, %d: Breakpoint size.
 __( '%s (%dpx and up)', 'elementor' );
 // translators: %s: Breakpoint label, %d: Breakpoint size.
 __( '%s (up to %dpx)', 'elementor' );
-__( 'Keyboard Shortcuts', 'elementor' );
+__( 'Save Changes', 'elementor' );
 __( 'History', 'elementor' );
+__( 'Keyboard Shortcuts', 'elementor' );
 __( 'Finder', 'elementor' );
 __( 'Elements', 'elementor' );
 __( 'Widgets', 'elementor' );
@@ -23,10 +23,12 @@ __( 'View Page', 'elementor' );
 __( 'Save as Template', 'elementor' );
 __( 'Save Draft', 'elementor' );
 __( 'Copy and Share', 'elementor' );
+__( 'Preview Changes', 'elementor' );
 __( 'Save Options', 'elementor' );
 __( 'Save Options', 'elementor' );
 __( 'Publish', 'elementor' );
 __( 'Submit', 'elementor' );
-__( 'Preview Changes', 'elementor' );
+__( 'My Elementor', 'elementor' );
+__( 'Connect my account', 'elementor' );
 /* translators: %s: Post type label. */
 __( '%s Settings', 'elementor' );
