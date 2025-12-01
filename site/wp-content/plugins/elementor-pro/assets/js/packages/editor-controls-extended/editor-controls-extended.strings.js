73b957d495ea7f80e5eb39ec2b4c8f654d84662e
rivercityinternetgroup <github@primary.net>
2025-12-02 02:29:23 +0800

    Updated/Added simply-static-1-1764609990

--- site/wp-content/plugins/elementor-pro/assets/js/packages/editor-controls-extended/editor-controls-extended.strings.js
+++ site/wp-content/plugins/elementor-pro/assets/js/packages/editor-controls-extended/editor-controls-extended.strings.js
@@ -62,3 +62,12 @@ __(
 				'Names can only use letters, numbers, dashes (-) and underscores (_).',
 				'elementor-pro'
 			);
+__(
+			'The use of pseudo-states is not permitted. Instead, switch to the desired pseudo state and add your custom code there.',
+			'elementor-pro'
+		);
+__(
+			'The use of @media width queries is not permitted. Instead, switch to the desired breakpoint and add your custom code there.',
+			'elementor-pro'
+		);
+__( 'Clear', 'elementor-pro' );
